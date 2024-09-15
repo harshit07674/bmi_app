395a052d26dabf3b0ac6872623420dbd9ab5ba2f
harshit07674 <harshitkhandelwal0705@gmail.com>
2024-09-15 16:52:43 +0530

    Delete lib/utils/api_constants.dart

--- lib/utils/api_constants.dart
+++
@@ -1,5 +0,0 @@
-class ApiConstants{
-
- static const String bearerToken = 'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJVbWVzaCIsInJvbGVJZCI6MiwiZXhwIjoxNzU0NDU1MDY5LCJpYXQiOjE3MjI5MTkwNjl9.WwBZtiNMQAeerkqkQL2MQjNVyfAEn7gE8CzzU0XpCIE';
- static const apiBaseUrl = "http://157.15.202.186:7002/patient/patient_info?patientId=73";
-}
